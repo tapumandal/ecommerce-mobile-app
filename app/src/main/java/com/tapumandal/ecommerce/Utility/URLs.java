401d450afd53cd5adc0ad7f73285c8f36c058aec
Tapu Mandal <online.tapu@gmail.com>
2020-10-29 09:14:05 +0600

    api calling is done

--- app/src/main/java/com/tapumandal/ecommerce/Utility/URLs.java
+++ app/src/main/java/com/tapumandal/ecommerce/Utility/URLs.java
@@ -3,8 +3,8 @@ package com.tapumandal.ecommerce.Utility;
 public class URLs {
 
 //    public static String ROOT_URL_MAIN = "";
-    public static String ROOT_URL_MAIN = "http://192.168.0.100:8080/v1/api/";
-    public static boolean isLive = false;
+    public static String ROOT_URL_MAIN = "http://192.168.0.100:8080/api/v1/";
+    public static boolean isLive = true;
 
 
 }

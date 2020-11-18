adfd913200db9dd91aeaf7b04e7f18f653deb377
Tapu Mandal <online.tapu@gmail.com>
2020-11-19 03:23:31 +0600

    Address layout design done

--- app/src/main/java/com/tapumandal/ecommerce/Activity/Order/CheckoutActivity.java
+++ app/src/main/java/com/tapumandal/ecommerce/Activity/Order/CheckoutActivity.java
@@ -1,8 +1,12 @@
 package com.tapumandal.ecommerce.Activity.Order;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.lifecycle.ViewModelProviders;
+import com.google.gson.Gson;
 import com.tapumandal.ecommerce.Base.BaseActivity;
 import com.tapumandal.ecommerce.R;
 import com.tapumandal.ecommerce.ViewModel.ProductControlViewModel;
@@ -30,6 +34,24 @@ public class CheckoutActivity extends BaseActivity {
         viewModel = ViewModelProviders.of(this).get(ProductControlViewModel.class);
 
         setToolbar("Checkout", true);
-//        setToolbar("Checkout");
+        initAreaSpinner();
+    }
+
+    private void initAreaSpinner() {
+        ArrayAdapter<String> filterAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.area_array));
+        binding.areaSpinner.setAdapter(filterAdapter);
+        binding.areaSpinner.setSelection(-1);
+        binding.areaSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
+                String s = binding.areaSpinner.getSelectedItem().toString();
+                System.out.println(i+"--"+s);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
